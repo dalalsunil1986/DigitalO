45bbd93d019a7712174fc26ce2e4faa502cbb250
Salvador <salvadorvalverde0@gmail.com>
2019-12-06 13:23:39 -0700

    S

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'git@github.com:salvadordeveloper/DigitalO.git');
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('167.71.103.118')
+    ->user('deployer')
+    ->identityFile('C:\ServerSSH\key')
+    ->set('deploy_path', '/var/www/html/laravel-app');
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+//before('deploy:symlink', 'artisan:migrate');
+
